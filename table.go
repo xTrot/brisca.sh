b9a9442d10a557c39fe2e02a4d9db51fb19c7404
xTrot <enddyygf93@live.com>
2025-01-18 20:54:36 -0500

    Added table model.

---
+++ table.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type tableModel struct {
+	deckSize    int
+	suitCard    card
+	cardsInPlay []card
+}
+
+func newTableModel() tableModel {
+	return tableModel{
+		deckSize: 40,
+		suitCard: newCard("BASTO", 3),
+		cardsInPlay: []card{
+			newCard("ESPADA", 1),
+			newCard("ORO", 5),
+			newCard("COPA", 10),
+		},
+	}
+}
+
+func (tm tableModel) Init() tea.Cmd {
+	return nil
+}
+
+func (tm tableModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return tm, nil
+}
+
+func (tm tableModel) renderCardsInPlay() string {
+	cip := ""
+	for i := range len(tm.cardsInPlay) {
+		cip += fmt.Sprintf("%s ", renderCard(tm.cardsInPlay[i]))
+	}
+	return cip
+}
+
+func (tm tableModel) View() string {
+	return fmt.Sprintf("Table:\n  Deck: %d\n  Suit Card: %s\n\n  In Play:\n  %s",
+		tm.deckSize, renderCard(tm.suitCard), tm.renderCardsInPlay())
+}
