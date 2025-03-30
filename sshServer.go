180f608344c28c4f603459a063e6be481c27a7e3
xTrot <enddyygf93@live.com>
2025-03-30 09:11:43 -0400

    Added scaffoliding for remembering pubKeys.

--- sshServer.go
+++ sshServer.go
@@ -6,8 +6,11 @@ import (
 	"net"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
+
+	gossh "golang.org/x/crypto/ssh"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/log"
@@ -18,6 +21,13 @@ import (
 	"github.com/charmbracelet/wish/logging"
 
 	"github.com/kelseyhightower/envconfig"
+)
+
+var (
+	db = map[string]string{
+		"Enddy": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCa4MMeQKYeAyEekyGOA0WlD/vFvlxQXu/yZV81wMWEKeplLTvMQGjfpsgA51BvmLAAzFhBnPloAFS6+dUvKlYJ27HbdI/6hLAxUPH19OwYD9Aks0utGIXqPRwPl+TVCM+4OZkvsd18rKueWJ/SYBeVudNzXECx9UmB2n33Rz4OLC+tKLuoYVgAd3LGIHsRS29o67OpPwHdW8zosCfQ6ZLD4oAinHqSIZCqfXXtrfee1F5hQNTxPTU47zyVNUshm9JaqrYQyFn5AWKjolcqOr3zt176rULsphZYcha9XpZ6u2M0YeJEkLUIrKAVoY3aTG0ZqBKMryLA4G89L+AQCcX1lxvMnU1SOotQ57C/CDC+iiqWF1VguU/23H80LVANYEenJYqgPhN3A42d7HchcaW8VTAwjLrrBSPT9F336oi+jQNGTPWfNndp9i0dlbPZbSRW89hOX+N5EHodWZPf09Wb5pvNm2Hyd2GCIoIhmF+kMyJnP3X6LX9ebn5jKK9mxiqsdjvZKlQLlhaLGHZmMJKyqWzOPKcBFPzU+h9/pUrqcmNQCP8djM8Al7z/JLeUqP9TIXv7W/yyVvMcAA/TgR/GvcXwV+JQFqi4x2EwaD+VMQeSBvGu01f0CVPZFepmDLBpSztzr0adk6f49aCNwXY+52s/Z3VxTjNblStqLFYAFw== enddyygf93@live.com",
+	}
+	allowedKeyTypes = "ssh-rsa, "
 )
 
 type Environment struct {
@@ -38,9 +48,13 @@ func main() {
 	s, err := wish.NewServer(
 		wish.WithAddress(net.JoinHostPort(env.Host, env.Port)),
 		wish.WithHostKeyPath(".ssh/id_ed25519"),
+		wish.WithPublicKeyAuth(keyHandler),           // This should be optional but isn't.
+		wish.WithKeyboardInteractiveAuth(skipThis()), // If this isn't added the PubKeyAuth will require a key.
+		// This makes make PubKey Auth optional
 		wish.WithMiddleware(
 			bubbletea.Middleware(teaHandler),
 			activeterm.Middleware(), // Bubble Tea apps usually require a PTY.
+			AuthMiddleware(),
 			logging.Middleware(),
 		),
 	)
@@ -76,6 +90,12 @@ func main() {
 	}
 }
 
+func skipThis() ssh.KeyboardInteractiveHandler {
+	return func(ctx ssh.Context, challenger gossh.KeyboardInteractiveChallenge) bool {
+		return true
+	}
+}
+
 // You can wire any Bubble Tea model up to the middleware with a function that
 // handles the incoming ssh.Session. Here we just grab the terminal info and
 // pass it to the new model. You can also return tea.ProgramOptions (such as
@@ -95,4 +115,49 @@ func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
 
 	m := newModel(&s)
 	return m, []tea.ProgramOption{tea.WithAltScreen()}
+}
+
+type keyMiddleware wish.Middleware
+
+func keyHandler(ctx ssh.Context, key ssh.PublicKey) bool {
+	if !strings.Contains(allowedKeyTypes, key.Type()) {
+		allowedKeyTypes += key.Type() + ", "
+		log.Info("newKeyTypeAdded:", "allowedKeyTypes", allowedKeyTypes)
+	}
+	return true
+}
+
+func AuthMiddleware() wish.Middleware {
+	return func(next ssh.Handler) ssh.Handler {
+		return func(sess ssh.Session) {
+			keyUserGave := sess.PublicKey()
+
+			if keyUserGave == nil {
+				log.Info("AuthMiddleware: No key provided.")
+				next(sess)
+				return
+			}
+
+			if !strings.Contains(allowedKeyTypes, keyUserGave.Type()) {
+				allowedKeyTypes += keyUserGave.Type() + ", "
+				log.Info("newKeyTypeAdded:", "allowedKeyTypes", allowedKeyTypes)
+			}
+
+			var found bool
+			for name, pubkey := range db {
+				keyStored, _, _, _, _ := ssh.ParseAuthorizedKey([]byte(pubkey))
+				if ssh.KeysEqual(keyUserGave, keyStored) {
+					log.Info("AuthMiddleWare: I remember,", "name", name)
+					found = true
+				}
+			}
+
+			if !found {
+				log.Info("AuthMiddleware: I don't remember, I can offer to remember you!", "name", sess.User())
+			}
+
+			next(sess)
+			return
+		}
+	}
 }
